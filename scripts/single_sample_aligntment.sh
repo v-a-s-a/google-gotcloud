619fa83c8e96cdc1a0a49b2bb0f9355d8ceea135
vtrubets <trubetsk@umich.edu>
2015-05-15 17:34:36 +0000

    GotCloud config and script for alignment of single sample

---
+++ scripts/single_sample_aligntment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##############################################################################
+#### GotCloud Alignment of HG01051
+##############################################################################
+
+gotcloud align \
+	--conf /home/trubetsk/google-gotcloud/config/single_sample_aligntment.gotcloud.conf \
+	--numjobs 1
+
