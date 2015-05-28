b52bda842b491cf7a26ccee726c5da8f6ac5142b
vtrubets <trubetsk@umich.edu>
2015-05-28 15:12:30 +0000

    script for running snpcall on one 1000G sample

---
+++ scripts/single_sample_variant-calling.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##############################################################################
+#### GotCloud Alignment of HG01051
+##############################################################################
+
+gotcloud snpcall \
+	--conf /home/trubetsk/google-gotcloud/config/single_sample_aligntment.gotcloud.conf \
+	--numjobs 1
+
