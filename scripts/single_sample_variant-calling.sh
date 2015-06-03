1063a9ebeda102f5d2c9fab064f418976e24149c
vtrubets <trubetskoy.vasa@gmail.com>
2015-06-03 20:43:04 +0000

    included recommended options for single variant calling

--- scripts/single_sample_variant-calling.sh
+++ scripts/single_sample_variant-calling.sh
@@ -4,7 +4,7 @@
 #### GotCloud Alignment of HG01051
 ##############################################################################
 
-gotcloud snpcall \
+/home/vasya/gotcloud/gotcloud snpcall \
 	--conf /home/trubetsk/google-gotcloud/config/single_sample_aligntment.gotcloud.conf \
-	--numjobs 1
+	--numjobs 16
 
