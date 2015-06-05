21cdb2424f76375ca43be94ea155ca0c701e4571
vtrubets <trubetskoy.vasa@gmail.com>
2015-06-05 14:53:04 +0000

    multisample snpcall update

---
+++ scripts/multi_sample_variant_calling.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##############################################################################
+#### GotCloud Alignment of HG01051
+##############################################################################
+
+/home/vasya/gotcloud/gotcloud snpcall \
+	--conf /home/trubetsk/google-gotcloud/config/multi_sample_snpcall.conf \
+	--numjobs 16
+
