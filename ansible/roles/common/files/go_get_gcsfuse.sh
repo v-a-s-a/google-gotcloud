1ad4ddb95e9546ce6bb34e3fa2740b06e311dbeb
vasya <trubetskoy.vasa@gmail.com>
2015-07-08 13:46:30 -0400

    update

--- ansible/roles/common/files/go_get_gcsfuse.sh
+++ ansible/roles/common/files/go_get_gcsfuse.sh
@@ -3,5 +3,6 @@ export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
 if [ ! -d "/home/trubetsk/go/src/github.com/googlecloudplatform/gcsfuse" ]; then
-  /usr/local/go/bin/go get -u github.com/googlecloudplatform/gcsfuse
+  sleep 1s
+  #  /usr/local/go/bin/go get -u github.com/googlecloudplatform/gcsfuse
 fi
---
+++ scripts/multi_sample_alignment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gotcloud align \
+	--conf /home/trubetsk/google-gotcloud/config/single_sample_aligntment.gotcloud.conf \
+	--numjobs 1
+
