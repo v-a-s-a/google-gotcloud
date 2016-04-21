67846987ccb298683e59ee1f7332b9cead124541
vasya <trubetskoy.vasa@gmail.com>
2016-04-20 23:19:14 -0400

    Fixed my call to google genomics.

--- google_gatk/1000g_test_align.sh
+++ google_gatk/1000g_test_align.sh
@@ -5,7 +5,7 @@ population="GBR"
 sequence_type="Genome"
 
 for i in {1..2}; do
-  gatk-google-alpha set Sample:"${sample}" Run:"${run}" Population:"${population}" Platform:Illumina SequenceType:"${sequence_type}"  gs://SRR062634_${i}.filt.fastq.gz 
+  gatk-google-alpha set Sample:"${sample}" Run:"${run}" Population:"${population}" Platform:Illumina SequenceType:"${sequence_type}"  gs://gatk-testing/fastq/SRR062634_${i}.filt.fastq.gz 
 done
 
-gatk-google-alpha dna-align-3.4-0a -project-id friendly-medley-91616 -input gs://gatk-testing/*fastq.gz -output gs://gatk-testing/output/
+gatk-google-alpha dna-align-3.4-0a -project-id friendly-medley-91616 -input gs://gatk-testing/fastq/*fastq.gz -output gs://gatk-testing/output/
--- google_gatk/gatk_variant_calling_24_wgs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-time gatk-google-alpha dna-genotype-3.4-0a -project friendly-medley-91616 -input gs://gs-fuse-test/bams.*.bam -output gs://gs-fuse-test/gatk_output/
-
